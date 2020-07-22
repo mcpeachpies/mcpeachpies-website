78683c99e3b2f7e92a04391f3dc3f2e590cae6c9
Jip Fr <jipfrijlink@gmail.com>
2020-07-22 12:17:40 +0200

    Datapack, oops

--- datapack/index.php
+++ datapack/index.php
@@ -1,57 +1,37 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Peach site</title>
-		<link rel="stylesheet" href="../style.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	</head>
-	<body>
-		
-		<!-- HERO -->
-		<header>
-			<div class="bg"></div>
-			<nav>
-				<a href="/">Home</a>
-				<a href="https://existencesmp.com/" target="_blank">Existence</a>
-				<a href="#">Datapacks</a>
-				<a href="#">Other link</a>
-				<a href="#">Idk</a>
-			</nav>
-		</header>
+<?php include("../start.php") ?>
 
-		<!-- MAIN PAGE -->
-		<div class="wrapper">
-			<div class="content layout">
-				<div class="content-core">
-					<h2 class="title skeleton">Fake title</h2>
-					<div class="youtube-video skeleton">
-						
-					</div>
-				
-					<p class="description skeleton">This is my description</p>
+<!-- MAIN PAGE -->
+<div class="wrapper">
+	<div class="content layout">
+		<div class="content-core">
+			<h2 class="title skeleton">Fake title</h2>
+			<div class="youtube-video skeleton">
 
-					<h3 class="sub-title skeleton">Features</h3>
-					<ul class="features">
-						<li class="skeleton">da jsdhd awdjwadow djwaijd iaso djaw</li>
-						<li class="skeleton">d awkdj wapoid oaelkjfslfs cm,xnzc ,mszcxzmczjwi dwaid; askldjkal jdwio;daslj.dawopd;aw</li>
-						<li class="skeleton">dasd jsadwahj kdahw doiawdjsalkdjawkdahkwhd asawljd kasjd</li>
-					</ul>
-				
-				</div>
-				<aside class="content-aside">
-					<div class="downloads">
-						<h2 class="title">Downloads</h2>
-						<div class="buttons">
-							<a href="#" class="download-button skeleton">Download for 1.16</a>
-							<a href="#" class="download-button skeleton">Download for 1.15</a>
-						</div>
-					</div>
-				</aside>
 			</div>
+
+			<p class="description skeleton">This is my description</p>
+
+			<h3 class="sub-title skeleton">Features</h3>
+			<ul class="features">
+				<li class="skeleton">da jsdhd awdjwadow djwaijd iaso djaw</li>
+				<li class="skeleton">d awkdj wapoid oaelkjfslfs cm,xnzc ,mszcxzmczjwi dwaid; askldjkal jdwio;daslj.dawopd;aw</li>
+				<li class="skeleton">dasd jsadwahj kdahw doiawdjsalkdjawkdahkwhd asawljd kasjd</li>
+			</ul>
+
 		</div>
+		<aside class="content-aside">
+			<div class="downloads">
+				<h2 class="title">Downloads</h2>
+				<div class="buttons">
+					<a href="#" class="download-button skeleton">Download for 1.16</a>
+					<a href="#" class="download-button skeleton">Download for 1.15</a>
+				</div>
+			</div>
+		</aside>
+	</div>
+</div>
 
-		<script src="../repo.js"></script>
-		<script src="datapack.js"></script>
+<script src="../repo.js"></script>
+<script src="datapack.js"></script>
 
-	</body>
-</html>
+<?php include("../end.php") ?>
