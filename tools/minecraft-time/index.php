385c8f33fc14a7ad5b77ddb5dd8814266f985f1f
Jip Fr <jipfrijlink@gmail.com>
2020-07-22 12:57:56 +0200

    Minecraft time calculator

---
+++ tools/minecraft-time/index.php
@@ -0,0 +1,36 @@
+<?php include("../../start.php") ?>
+
+<!-- MAIN PAGE -->
+<div class="wrapper">
+	<div class="content extra-wide">
+		<div class="content-core">
+
+			<h1 class="title">Real time to Minecraft time converter</h1>
+			<p>by mcpeachpies</p>
+
+			<div class="input-div">
+				<label for="time">Insert time in 24hrs <sup id="example">(e.g. 0600 = 6am)</sup></label>
+				<div class="nowrap-div">
+					<input name="time" type="text" value="0600" id="time" /> 
+					<input type="button" value="Submit" onclick="calculate(this.form)" />
+				</div>
+			</div>
+
+
+			<div class="output-wrapper">
+				<h3 class="sub-title">Result</h3>
+				<p id="output">(( hours * 1000 ) - 6000 ) + ( minutes * 16 ) = daytime</p>
+			</div>
+
+			<div class="input-div nowrap-div">
+				<input type="text" id="command" value="/time set daytime" readonly />
+				<input type="button" value="Copy command" id="copy" /> <input type="button" value="Refrest current time" onclick="example()" />
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<script src="/tools/minecraft-time/time.js"></script>
+
+<?php include("../../end.php") ?>
---
+++ tools/minecraft-time/time.js
@@ -0,0 +1,64 @@
+function innerHTML(element, text) {
+	document.getElementById(element).innerHTML = text;
+}
+
+function example() {
+
+	var date = new Date();
+
+	var hours = (date.getHours()).toString();
+	var T_hours = hours;
+
+	if (T_hours > 11) {
+		var meridiem = "pm";
+		T_hours -= 12
+
+	} else { var meridiem = "am" };
+
+	if (hours < 10) { hours = "0" + hours }
+
+	var minutes = (date.getMinutes()).toString();
+	if (minutes < 10) { minutes = "0" + minutes }
+
+	var currentTime = hours + minutes;
+	convert(currentTime);
+	document.getElementById("time").value = currentTime;
+
+	innerHTML("example", "(e.g. " + currentTime + " = " + T_hours + ":" + minutes + meridiem + ")")
+}
+
+function calculate(form) {
+
+	var fullTime = document.querySelector(`[name="time"]`).value;
+	convert(fullTime);
+
+}
+
+function convert(fullTime) {
+
+	var Chours = fullTime.slice(0, 2);
+	var Cminutes = fullTime.slice(2, 4);
+
+	if (Chours < 06) {
+		var daytime = Math.ceil(((Chours * 1000) + 18000) + (Cminutes * 16.7));
+		innerHTML("output", "(( " + Chours + "* 1000 ) + 18000 ) + ( " + Cminutes + "* 16.7 ) = " + daytime);
+		document.getElementById("command").value = "/time set " + daytime;
+	}
+	else {
+		var daytime = Math.ceil(((Chours * 1000) - 6000) + (Cminutes * 16.7));
+		innerHTML("output", "(( " + Chours + "* 1000 ) - 6000 ) + ( " + Cminutes + "* 16.7 ) = " + daytime);
+		document.getElementById("command").value = "/time set " + daytime;
+	}
+
+
+}
+
+var copyButton = document.getElementById("copy");
+
+copyButton.addEventListener('click', function (event) {
+	var commandArea = document.getElementById("command");
+	commandArea.select();
+
+	document.execCommand('copy');
+
+});
