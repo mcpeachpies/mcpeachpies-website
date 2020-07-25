bd14fa05aa33ada4afe75fc6045e1c0d7c5510bd
Jip Fr <jipfrijlink@gmail.com>
2020-07-25 12:03:48 +0200

    Add "credits" footer

--- end.php
+++ end.php
@@ -1,3 +1,7 @@
+		</div>
+		<footer>
+			<p>Website built and maintained by <a href="https://jipfr.nl" target="_blank" class="read-more">Jip Fr</a></p>
+		</footer>
 
 	</body>
 </html>
--- start.php
+++ start.php
@@ -6,28 +6,30 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	</head>
 	<body>
-		
-		<!-- HERO -->
-		<header>
-			<div class="bg"></div>
-			<nav>
-				<a href="/" class="home-link">Home</a>
-				<a href="/resource-pack" class="resource-pack-link">Resource Pack</a>
-				<a href="https://existencesmp.com/" target="_blank">Existence</a>
-			</nav>
-		</header>
 
-		<!-- TEMPLATES -->
-		<div class="templates" style="display: none">
-			<template class="card">
-				<div class="card">
-					<div class="card-top">
-						<h3 class="title"><img class="pack-icon" onerror="this.remove();"><span class="title-text">This is a card's title</span></h3>
-						<p class="description">atea ad.rabitur habitant sapien dis montes senectus mollis ut lectus</p>
+		<div class="content-wrapper">
+			
+			<!-- HERO -->
+			<header>
+				<div class="bg"></div>
+				<nav>
+					<a href="/" class="home-link">Home</a>
+					<a href="/resource-pack" class="resource-pack-link">Resource Pack</a>
+					<a href="https://existencesmp.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>Existence</a>
+				</nav>
+			</header>
+
+			<!-- TEMPLATES -->
+			<div class="templates" style="display: none">
+				<template class="card">
+					<div class="card">
+						<div class="card-top">
+							<h3 class="title"><img class="pack-icon" onerror="this.remove();"><span class="title-text">This is a card's title</span></h3>
+							<p class="description">atea ad.rabitur habitant sapien dis montes senectus mollis ut lectus</p>
+						</div>
+						<div class="card-bottom">
+							<a class="read-more" href="#">Learn more</a>
+						</div>
 					</div>
-					<div class="card-bottom">
-						<a class="read-more" href="#">Learn more</a>
-					</div>
-				</div>
-			</template>
-		</div>
+				</template>
+			</div>
