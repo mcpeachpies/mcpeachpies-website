8ef64558b3c1a8cf375bf192dda0a2a13809d2c7
Jip Fr <jipfrijlink@gmail.com>
2020-08-07 09:14:04 +0200

    Update "tool" texts

--- include/tools.php
+++ include/tools.php
@@ -4,7 +4,7 @@
 		<div class="card">
 			<div class="card-top">
 				<h3 class="title"><img class="pack-icon" src="/tools/skin-viewer/assets/head.php/?s=200&u=mcpeachpies" onerror="this.remove();"><span class="title-text">Skin viewer</span></h3>
-				<p class="description">atea ad.rabitur habitant sapien dis montes senectus mollis ut lectus</p>
+				<p class="description">View people's Minecraft skin online</p>
 			</div>
 			<div class="card-bottom">
 				<a class="read-more" href="/tools/skin-viewer/">Learn more</a>
@@ -13,7 +13,7 @@
 		<div class="card">
 			<div class="card-top">
 				<h3 class="title"><img class="pack-icon" src="https://raw.githubusercontent.com/mcpeachpies/mcpeachpies-resource-pack/master/%C2%A79mcpeachpies%20%C2%A7f1.16/assets/minecraft/textures/environment/sun.png" onerror="this.remove();"><span class="title-text">Minecraft time calculator</span></h3>
-				<p class="description">atea ad.rabitur habitant sapien dis montes senectus mollis ut lectus</p>
+				<p class="description">Convert 24-hour time to Minecraft time</p>
 			</div>
 			<div class="card-bottom">
 				<a class="read-more" href="/tools/minecraft-time/">Learn more</a>
