a8b0c1952d38b09c7fb0bf9e9c72b98dd75a3160
mcpeachpies <mcpeachpies@gmail.com>
2020-08-15 09:27:43 +1200

    Datapack description & Resource pack download
    Added description to datapack page
    
    Added alternative download for 1.15+ resource pack

--- resource-pack/index.php
+++ resource-pack/index.php
@@ -75,7 +75,8 @@
 			<div class="downloads">
 				<h2 class="title">Downloads</h2>
 				<div class="buttons">
-					<a href="https://jipfr.github.io/DownGit/#/home?url=https://github.com/mcpeachpies/mcpeachpies-resource-pack/tree/master/%C2%A79mcpeachpies%20%C2%A7f1.16" class="download-button skeleton" target="_blank">Download</a>
+					<a href="https://jipfr.github.io/DownGit/#/home?url=https://github.com/mcpeachpies/mcpeachpies-resource-pack/tree/master/%C2%A79mcpeachpies%20%C2%A7f1.16" class="download-button skeleton" target="_blank">Download (1.16.2)</a>
+					<a href="https://jipfr.github.io/DownGit/#/home?url=https://github.com/mcpeachpies/mcpeachpies-resource-pack/tree/master/%C2%A79mcpeachpies%20%C2%A7f1.15" class="download-button skeleton" target="_blank">Download (1.15+)</a>
 				</div>
 			</div>
 		</aside>
