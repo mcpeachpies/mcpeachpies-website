df2b2699558fcb5cc0f01db6f54ca0dd345f6b5a
mcpeachpies <mcpeachpies@gmail.com>
2020-08-15 10:22:56 +1200

    Update to repo name

--- resource-pack/block.js
+++ resource-pack/block.js
@@ -1,3 +1,3 @@
 document.querySelectorAll(".block img").forEach(img => {
-	img.src = `https://raw.githubusercontent.com/mcpeachpies/mcpeachpies-resource-pack/master/§9mcpeachpies%20§f1.16${img.getAttribute("data-src")}`
+	img.src = `https://raw.githubusercontent.com/mcpeachpies/mcpeachpies-resource-pack/master/§9mcpeachpies%20§f1.16.2${img.getAttribute("data-src")}`
 });
