cd8e43314bd41b501bd9ad0238a6eadf70fd71cd
mcpeachpies <mcpeachpies@gmail.com>
2020-09-23 13:31:03 +1200

    Added ID for One-clicks
    http://mcpeachpies.com/datapacks#one-clicks

--- datapacks/list.php
+++ datapacks/list.php
@@ -22,10 +22,8 @@
 			<?php include("../include/datapack.php") ?>
 
 			<div class="section">
-				<h2 class="title">One-clicks for 1.12</h2>
+				<h2 class="title" id="one-clicks">One-clicks for 1.12</h2>
 				<p>Collection of Minecraft 1.12 One-Click Command block modules. These are now archived and no longer supported, but do join my <a href="https://discord.gg/g7DxdVg" target="_blank" class="read-more">Discord</a> server if you have an questions or problems</p>
-				<!-- Hi Jip, could you make this fit side-by-side on the page please :) -->
-				<!-- Yep, of course! -->
 				<div class="frames">
 					<iframe class="list-frame" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLxWlnWpCLIWWGxZ1aw93ChX5GWCdoMwor" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 					<iframe class="list-frame" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLxWlnWpCLIWV0g3Q-t7tRIylHwamyGjF8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
